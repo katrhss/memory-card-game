2c2328cf89a2b6a36fcbd40ec0eafd10bbc36461
Konstantinos Katris <93059645+katrhss@users.noreply.github.com>
2023-11-13 22:24:33 +0200

    Added full functionality

--- src/App.js
+++ src/App.js
@@ -1,33 +1,76 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+const icons = ["🐛", "💰", "🎓", "📎", "💻", "🕸️", "🌟", "🐈"];
+
 function App() {
   const [cards, setCards] = useState([]);
-  const [showCard, setShowCard] = useState(false);
-  const [selectedCard, setSelectedCard] = useState();
+  const [matchedCards, setMatchedCards] = useState([]);
+  const [flippedIndexes, setFlippedIndexes] = useState([]);
+
+  const text = matchedCards.length !== 16 ? "Memory Card Game" : "You won!";
 
   function handleClick(index) {
-    setSelectedCard(index);
+    if (flippedIndexes.length === 0) setFlippedIndexes([index]);
+
+    if (
+      flippedIndexes.length === 1 &&
+      flippedIndexes[0] !== index &&
+      cards[flippedIndexes[0]] === cards[index]
+    ) {
+      setMatchedCards([
+        ...matchedCards,
+        cards[flippedIndexes[0]],
+        cards[index],
+      ]);
+    }
+
+    if (flippedIndexes.length === 1) {
+      console.log("Im here");
+      setFlippedIndexes([flippedIndexes[0], index]);
+      setTimeout(() => {
+        setFlippedIndexes([]);
+      }, 1000);
+    }
+  }
+
+  function handleReset() {
+    setCards(icons.concat(icons).sort(() => Math.random() - 0.5));
+    setFlippedIndexes([]);
+    setMatchedCards([]);
   }
 
   useEffect(() => {
-    const icons = ["🐛", "💰", "🎓", "📎", "💻", "🕸️", "🌟", "🐈"];
     setCards(icons.concat(icons).sort(() => Math.random() - 0.5));
   }, []);
 
   return (
     <div className="App">
+      <h2 className={matchedCards.length === 16 ? "victory-text" : "text"}>
+        {text}
+      </h2>
       <div className="container">
         {cards.map((card, index) => (
           <div
-            className={`card ${selectedCard === index ? "selected" : "empty"}`}
+            className={`card ${
+              flippedIndexes.includes(index) ? "selected" : "empty"
+            } ${matchedCards.includes(card) ? "matched" : ""}`}
             key={index}
             onClick={() => handleClick(index)}
           >
-            {selectedCard === index && card}
+            {(flippedIndexes.includes(index) || matchedCards.includes(card)) &&
+              card}
           </div>
         ))}
       </div>
+
+      <button
+        className={matchedCards.length === 16 ? "reset-btn" : "btn-disabled"}
+        onClick={handleReset}
+        disabled={matchedCards.length === 16 ? false : true}
+      >
+        Reset
+      </button>
     </div>
   );
 }
